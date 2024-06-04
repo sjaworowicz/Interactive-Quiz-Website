3b157ff18ae0345b74e76d13f3abbc1386d3335f
sjaworowicz <125sjaworowicz@frhsd.com>
2024-06-04 08:33:19 -0400

    Worked mainly on JS

--- js/script.js
+++ js/script.js
@@ -89,4 +89,32 @@ const questions = [
             {text: '2003', correct: false},
         ]
     },
-]
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-button");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions [currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
