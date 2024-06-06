16938c882ca4788dd6455b41198c0abc6da1bbd0
sjaworowicz <125sjaworowicz@frhsd.com>
2024-06-06 10:48:13 -0400

    Got all the coding done basically, just have to fix up a few things and modified the site a bit

--- js/script.js
+++ js/script.js
@@ -140,6 +140,37 @@ function selectAnswer(e){
     }else{
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = 'You scored ${score} out of ${questions.length}!';
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+};
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+};
+
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
 
 startQuiz();
